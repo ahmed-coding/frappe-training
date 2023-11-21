ca48fe9a9fe80c760a3932d55d31059f42becf81
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2023-11-22 00:31:21 +0300

    create app
    create app Test and programmer_module with one module.

---
+++ apptest/apptest/programming_module/doctype/test_module/test_module.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ahmed-Coding and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Test module", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ apptest/apptest/test_apps/doctype/programming_module/programming_module.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ahmed-Coding and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Programming_module", {
+// 	refresh(frm) {
+
+// 	},
+// });
