eae00de54330b1134c7c828b021b46eda6c543b2
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2023-11-25 00:37:06 +0300

    work in client side

--- apptest/apptest/programming_module/doctype/doctype_test_1/doctype_test_1.js
+++ apptest/apptest/programming_module/doctype/doctype_test_1/doctype_test_1.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2023, Ahmed-Coding and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("DocType Test 1", {
-// 	refresh(frm) {
+frappe.ui.form.on("DocType Test 1", {
+	refresh(frm) {
+        frappe.msgprint('A row has been added to the DocType Test 1  table 🎉 ');
 
-// 	},
-// });
+	},
+	// onload(frm) {
+    //     frappe.msgprint('A row has been added to the DocType Test 1  table 🎉 ');
+
+    // },
+    // refresh(frm)
+});
